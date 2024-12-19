962338e2d0835236acef6e1c7f1b0ec09f463f0f
Viren9769 <133132357+Viren9769@users.noreply.github.com>
2024-12-19 09:59:04 -0600

    Initial frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+//import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
+  // constructor
+  http = inject(HttpClient);
+  title = 'Gunatit Sambandh';
+  users: any;
+ // constructor(private http: HttpClient){}
+ // life cycle hook
+ ngOnInit(): void {
+  this.http.get('http://localhost:5000/api/user').subscribe({
+    next: response => this.users = response,
+    error: error => console.log(error),
+    complete: () => console.log('Request has completed')  // handle completion of HTTP request
+  })
+ }
 }
+  
